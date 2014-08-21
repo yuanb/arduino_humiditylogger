f48e3eb03f5384ce83429668bb34101ebd138d1b
yuanb <billyuan@gmail.com>
2014-08-20 22:57:10 -0400

    added support for reading DS1820 sensor.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 arduino_humiditylogger
 ======================
 This is a simple humidity logger based on Arduino UNO and HIH-4030.
-Original based on Arduino Super Graphing Data Logger
+Based on Arduino Super Graphing Data Logger
 http://everettsprojects.com/2012/12/31/arduino-super-graphing-data-logger/
+
+and 
+
+http://tutorialpedia.org/tutorials/Working+with+Dallas+DS18S20+and+DS18B20+temperature+sensors+from+Arduino.html
--- arduino_humiditylogger.ino
+++ arduino_humiditylogger.ino
@@ -4,6 +4,7 @@
 #include <Time.h>
 //#include <Timezone.h>
 #include <EEPROM.h>
+#include <OneWire.h>
 #include <avr/wdt.h>
 #include "EEPROMAnything.h"
 
@@ -26,7 +27,13 @@ unsigned long ntpTime = 0;
 
 EthernetServer server(80);
 EthernetUDP Udp;
-  
+
+OneWire  ONE_WIRE_BUS(2);  // Bus One-Wire (sonda DS18S20) sul pin 10
+
+// Indirizzo della sonda DS18S20
+// Nota: trova l'indirizzo della tua sonda col programma precedente e incollalo al posto di questo
+byte T_SENSOR[8] = {0x10, 0x8F, 0x43, 0x37, 0x00, 0x08, 0x00, 0x2A};
+
 int freeRam () 
 {
   extern int __heap_start, *__brkval; 
@@ -134,16 +141,10 @@ void ListFiles(EthernetClient client) {
 // which in our case is UTC represented as unix time (choosen because it 
 // works simply with highcharts without too much unecessary computation).
 
-unsigned long getNtpTime() {
+unsigned long getNtpTime(byte* packetBuffer)
+{
   unsigned long ret = 0;
 
-  /************** NTP STUFF ***************/ 
-  byte* packetBuffer = (byte *)malloc(NTP_PACKET_SIZE * sizeof(byte));
-  
-  if (!packetBuffer) {
-    return 0;
-  }
-  
   if ( Udp.parsePacket() ) {  
     // We've received a packet, read the data from it
     Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read the packet into the buffer
@@ -170,22 +171,13 @@ unsigned long getNtpTime() {
     //ret = usEastern.toLocal(epoch);
     ret = epoch;
   }
-
-  if (packetBuffer) {
-    free(packetBuffer);
-  }
   return ret;  
 }
 
 // send an NTP request to the time server at the given address,
 // necessary for getTime().
-void sendNTPpacket(IPAddress& address)
+void sendNTPpacket(IPAddress& address, byte* packetBuffer)
 {
-  byte* packetBuffer = (byte *)malloc(NTP_PACKET_SIZE * sizeof(byte));
- 
-  if (!packetBuffer) {
-      return;
-  }
   // set all bytes in the buffer to 0
   memset(packetBuffer, 0, NTP_PACKET_SIZE); 
   // Initialize values needed to form NTP request
@@ -205,15 +197,7 @@ void sendNTPpacket(IPAddress& address)
   Udp.beginPacket(address, 123); //NTP requests are to port 123
   Udp.write(packetBuffer,NTP_PACKET_SIZE);
   Udp.endPacket();
-  
-  if (packetBuffer) {
-    free(packetBuffer);
-  }
 }
-
-// How big our line buffer should be for sending the files over the ethernet.
-// 75 has worked fine for me so far.
-#define BUFSIZ 75
 
 void generateNewFilename(unsigned long rawTime)
 {
@@ -261,7 +245,7 @@ void generateNewFilename(unsigned long rawTime)
 
 float getHumidity(float degreesCelsius){
   //caculate relative humidity
-  float supplyVolt = 5.0;
+  float supplyVolt = 4.7;
 
   // read the value from the sensor:
   int HIH4030_Value = analogRead(HIH4030_Pin);
@@ -285,9 +269,7 @@ void sampleSensorTask()
   {
     Serial.print(F("freeRam @ sampleSensor()="));
     Serial.print(freeRam());  
-    Serial.print(F(" lastIntervalTime="));
-    Serial.println(lastIntervalTime);
-  
+
     static int count = 0;
  
     if (ntpTime == 0 || ntpTime == 39) {
@@ -299,18 +281,21 @@ void sampleSensorTask()
     }
     
     if (ntpTime != 39)                          //If that worked, and we have a real time  
-    {        
+    {
       //Decide if it's time to make a new file or not. Files are broken
        //up like this to keep loading times for each chart bearable.
       //Lots of string stuff happens to make a new filename if necessary.
       if (ntpTime >= config.newFileTime)
       {
-        generateNewFilename(ntpTime);          
+        generateNewFilename(ntpTime);
       }
      
-      char dataString[20] = "";
+      float temperature = getTemperature(T_SENSOR);
+      Serial.print("temperature=");
+      Serial.println(temperature);
+      
+      char dataString[25] = "";
       //get the values and setup the string we want to write to the file
-      float temperature = 21.0; //replace with a thermometer reading if you have it
       float relativeHumidity  = getHumidity(temperature);
        
       char tempStr[12];
@@ -320,7 +305,10 @@ void sampleSensorTask()
       strcat(dataString,",");
 
       dtostrf(relativeHumidity,6, 2, tempStr);
-      strcat(dataString,tempStr);    
+      strcat(dataString,tempStr);
+      strcat(dataString,",");
+      dtostrf(temperature,6, 2, tempStr);
+      strcat(dataString,tempStr);
     
        //open the file we'll be writing to.
       File dataFile = SD.open(config.workingFilename, FILE_WRITE);
@@ -343,7 +331,13 @@ void sampleSensorTask()
   }
 }
 
-void serveClientTask() {
+void serveClientTask()
+{
+// How big our line buffer should be for sending the files over the ethernet.
+// 75 has worked fine for me so far.
+#define BUFSIZ 75
+  char clientline[75];
+
    // listen for incoming clients
    EthernetClient client = server.available();
    if (client)
@@ -351,12 +345,6 @@ void serveClientTask() {
       Serial.print(F("freeRam @ serveClient()="));
       Serial.println(freeRam());         
 
-      char* clientline = (char *)malloc( BUFSIZ * sizeof(char));
-   
-      if (!clientline) {
-        return ;
-      }
-        
       // reset the input buffer
       int index = 0;
 
@@ -447,11 +435,6 @@ void serveClientTask() {
     delay(1);
     // close the connection:
     client.stop();
-   
-    if (clientline)
-    {
-      free(clientline);
-    }
   }
 }
 
@@ -459,18 +442,64 @@ void updateTimeTask()
 {
   static byte eState = eStateTimeRead;
   static unsigned long taskMillis = 0;
+  byte packetBuffer[NTP_PACKET_SIZE];
   
   if (eState == eStateTimeRead && ((taskMillis == 0) || (millis() - taskMillis) >= 1000*20) ) {
     IPAddress timeServer(132, 163, 4, 101); //NIST time server IP address: for more info
                                             //see http://tf.nist.gov/tf-cgi/servers.cgi
-    sendNTPpacket(timeServer); // send an NTP packet to a time server
+    sendNTPpacket(timeServer, packetBuffer); // send an NTP packet to a time server
     eState = eStateNTPSent;
     taskMillis = millis();
   } else if (eState == eStateNTPSent && (millis() - taskMillis) >= 1000 ) {
-    ntpTime = getNtpTime();
+    ntpTime = getNtpTime(packetBuffer);
     eState = eStateTimeRead;
     taskMillis = millis();
   }
+}
+
+void writeTimeToScratchpad(byte* address)
+{
+  ONE_WIRE_BUS.reset();          // Reset 1 wire bus
+  ONE_WIRE_BUS.select(address);  // Select DS1820 sensor
+  ONE_WIRE_BUS.write(0x44,1);    // Richiamo la funzione di conversione temperatura (44h)
+  delay(1000);
+}
+ 
+void readTimeFromScratchpad(byte* address, byte* data)
+{
+  ONE_WIRE_BUS.reset();           // Resetto il bus
+  ONE_WIRE_BUS.select(address);   // Seleziono la mia sonda DS18S20
+  ONE_WIRE_BUS.write(0xBE);       // Comando di lettura dello scratchpad (comando Read Scratchpad)
+  for (byte i=0;i<9;i++)
+  {
+    data[i] = ONE_WIRE_BUS.read(); // Leggo i 9 bytes che compongono lo scratchpad
+  }
+}
+ 
+float getTemperature(byte* address)
+{
+  int tr;
+  byte data[12];
+  writeTimeToScratchpad(address); // Richiamo conversione temperatura
+  readTimeFromScratchpad(address,data); // effettuo la lettura dello Scratchpad
+  tr = data[0];   // Posiziono in TR il byte meno significativo
+  // il valore di temperatura è contenuto nel byte 0 e nel byte 1
+ 
+  // Il byte 1 contiene il segno
+  if (data[1] > 0x80) // Se il valore è >128 allora la temperatura è negativa
+  {
+    tr = !tr + 1; // Correzione per complemento a due
+    tr = tr * -1; // Ottengo il valore negativo
+  }
+ 
+  int cpc = data[7];   // Byte 7 dello scratchpad: COUNT PER °C (10h)
+  int cr = data[6];   // Byte 6 dello scratchpad: COUNT REMAIN (0Ch)
+  tr = tr >> 1;   // Rilascio il bit 0 come specificato dal datasheet per avere una risoluzione > di 9bit
+ 
+  // Calcolo la temperatura secondo la formula fornita sul datasheet:
+  // TEMPERATURA = LETTURA - 0.25 + (COUNT PER °C - COUNT REMAIN)/(COUNT PER °C)
+ 
+  return tr - (float)0.25 + (cpc - cr)/(float)cpc;
 }
 
 void idleTask()
